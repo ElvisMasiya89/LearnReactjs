37ae8d37ab30bd3183f30e558c29a1ffe82653ec
ElvisMRAMM <95755611+ElvisMRAMM@users.noreply.github.com>
2022-05-19 11:58:14 +0200

    first react app

---
+++ my-first-app/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (React.createElement('div', null, React.createElement('h1',null,'Hi, I\'m a Vcard App!!')));
+}
+export default App;
