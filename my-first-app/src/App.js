95a11aa00cb57e33511c988390b6b61a6024bcfa
ElvisMRAMM <95755611+ElvisMRAMM@users.noreply.github.com>
2022-05-19 21:12:19 +0200

    Update App.js

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import './App.css';
+import './App.css'; 
 
 function App() {
-  return (React.createElement('div', null, React.createElement('h1',null,'Hi, I\'m a Vcard App!!')));
+    // return(
+    //   <div className='App'>
+    //       <h1>Hi,I'm a Vcard App!!</h1> 
+    //   </div>
+    // )
+    //This code gets compiled down to this code
+
+   return (React.createElement('div', {className :"App"}, React.createElement('h1',null,'Hi, I\'m a Vcard App!!')));
 }
 export default App;
+ 
