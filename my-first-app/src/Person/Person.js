73343c191da80f37686970bfa10e024689d33846
ElvisMRAMM <95755611+ElvisMRAMM@users.noreply.github.com>
2022-06-05 00:18:50 +0200

    Styling Components CSS
    Styling Components CSS

--- my-first-app/src/Person/Person.js
+++ my-first-app/src/Person/Person.js
@@ -1,9 +1,10 @@
-import React  from 'react'
+import React  from 'react';
+import './Person.css';
 
 const person = (props) =>{
     //When using class-based components, its this.props
     return(
-      <div>
+      <div className = "Person">
         <p onClick={props.click}>l'm a {props.name} and l am {props.age} years old !</p>
         <p>{props.children}</p>  
         <input type='text' onChange={props.changed} value = {props.name}/>
