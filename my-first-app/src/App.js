fef54c0d1af96fa01e5ab335638c933b2ab96601
ElvisMRAMM <95755611+ElvisMRAMM@users.noreply.github.com>
2022-06-04 23:54:13 +0200

    adding two binding between components
    adding two binding between components

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,132 +1,126 @@
-// import React,{Component} from 'react'
-
-import React,{useState} from 'react' // import hook function that start with 'use' key word
+import React,{Component} from 'react'
+// import React,{useState} from 'react' // import hook function that start with 'use' key word
 import './App.css'; 
 import Person from './Person/Person'
 
-//React with Hooks
-const App = (props)=>{
+class App extends Component{
 
-  //Using array distructuring
-  const [personsState, setPersonState] =  useState({
-    //useState hook function returns an array with 2 items an state object and a function to modify state
-    persons:[
-            {name:'Max', age:28},
-            {name:'Manu', age:29},
-            {name:'Stephanie', age:26},
-          ],
-    })
-
-    const [otherState, setOtherState] =  useState({
-      //useState hook function returns an array with 2 items an state object and a function to modify state
-      otherState:'some other value' 
+    state = {
+      persons:[
+        {name:'Max', age:28},
+        {name:'Manu', age:29},
+        {name:'Stephanie', age:26},
+      ],
   
+    otherState:'some other value' 
+    }
+  
+  
+    switchNameHadler = (newName)=>{
+      console.log('Was Clicked')
+      //DO NO DO THIS personsState.persons[0].name = 'Princess'
+        this.setState( {persons: [
+          {name:newName, age:28},
+          {name:'Manu', age:29},
+          {name:'Danzel', age:26},]
+        } )
+    }
+
+      nameChangedHandler = (event)=>{
+      this.setState({persons: [
+        {name:"Wakanda", age:28},
+        {name:event.target.value, age:29},
+        {name:'Danzel', age:26},]
       })
-
-  //In JavaScript you can declare functions with other functions  
-  const switchNameHadler = ()=>{
-          console.log('Was Clicked')
-          //setPersonState does not merge the old state with the new 
-          //state as in class based componets instead it replace the old state,
-          //To manage different states,  use multiple setStates
-            setPersonState( {persons: [
-              {name:'Wakanda', age:28},
-              {name:'Manu', age:29},
-              {name:'Danzel', age:26},]
-            } )
-        }
-
-    return(
-      <div className='App'>
+    };    
+  
+    render(){
+      return(
+        <div className='App'>
           <h1>Hi,I'm a React App!!</h1> 
           <p>This is going really well!</p>
-          <button onClick={switchNameHadler}> Switch Name</button>
-          <Person name={personsState.persons[0].name}  age={personsState.persons[0].age}/>
-          <Person name = {personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-          <Person  name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
+          <button onClick={()=>this.switchNameHadler('Wakanda')}> Switch Name</button>
+          <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/>
+          <Person
+           name={this.state.persons[1].name}
+           age={this.state.persons[1].age}
+           click={this.switchNameHadler.bind(this,'Wakanda')}
+           changed = {this.nameChangedHandler}>My Hobbies: Racing
+           </Person>
+          <Person  name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
       </div> 
-      );
+        );
+    }
+
+
+
+
+
+
+
+
+  
+      //This code gets compiled down to this code
+    //  return (React.createElement('div', {className :"App"}, React.createElement('h1',null,'Hi, I\'m a Vcard App!!')));
   }
-export default App;
- 
+  export default App;
+   
 
+// //React with Hooks
+// const App = (props)=>{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class App extends Component{
-
-//   state = {
+//   //Using array distructuring
+//   const [personsState, setPersonState] =  useState({
+//     //useState hook function returns an array with 2 items an state object and a function to modify state
 //     persons:[
-//       {name:'Max', age:28},
-//       {name:'Manu', age:29},
-//       {name:'Stephanie', age:26},
-//     ],
+//             {name:'Max', age:28},
+//             {name:'Manu', age:29},
+//             {name:'Stephanie', age:26},
+//           ],
+//     })
 
-//   otherState:'some other value' 
-//   }
+//     const [otherState, setOtherState] =  useState({
+//       //useState hook function returns an array with 2 items an state object and a function to modify state
+//       otherState:'some other value' 
+  
+//       });
 
+//   //In JavaScript you can declare functions with other functions  
+//   const switchNameHadler = (newName)=>{
+//           console.log('Was Clicked')
+//           //setPersonState does not merge the old state with the new 
+//           //state as in class based componets instead it replace the old state,
+//           //To manage different states,  use multiple setStates
+//             setPersonState( {persons: [
+//               {name:newName, age:28},
+//               {name:'Manu', age:29},
+//               {name:'Danzel', age:26},]
+//             } )
+//         };
 
-//   switchNameHadler = ()=>{
-//     console.log('Was Clicked')
-//     //DO NO DO THIS personsState.persons[0].name = 'Princess'
-//       this.setState( {persons: [
-//         {name:'Wakanda', age:28},
-//         {name:'Manu', age:29},
-//         {name:'Danzel', age:26},]
-//       } )
-//   }
+//   // const nameChangedHandler = (event)=>{
+//   //     this.setState({persons: [
+//   //       {name:"Wakanda", age:28},
+//   //       {name:event.target.value, age:29},
+//   //       {name:'Danzel', age:26},]
+//   //     })
+//   //   };    
 
-//   render(){
 //     return(
 //       <div className='App'>
 //           <h1>Hi,I'm a React App!!</h1> 
 //           <p>This is going really well!</p>
-//           <button onClick={this.switchNameHadler}> Switch Name</button>
+//           <button onClick={()=>switchNameHadler('Wakanda')}> Switch Name</button>
 //           <Person name={personsState.persons[0].name}  age={personsState.persons[0].age}/>
-//           <Person name = {personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+//           <Person
+//            name={personsState.persons[1].name}
+//            age={personsState.persons[1].age}
+//            click={switchNameHadler.bind(this,'Waka!')}>My Hobbies: Racing
+//            </Person>
 //           <Person  name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
 //       </div> 
 //       );
 //   }
-
-//     //This code gets compiled down to this code
-//   //  return (React.createElement('div', {className :"App"}, React.createElement('h1',null,'Hi, I\'m a Vcard App!!')));
-// }
 // export default App;
  
+
--- my-first-app/src/Person/Person.js
+++ my-first-app/src/Person/Person.js
@@ -4,8 +4,9 @@ const person = (props) =>{
     //When using class-based components, its this.props
     return(
       <div>
-        <p>l'm a {props.name} and l am {props.age} years old !</p>
+        <p onClick={props.click}>l'm a {props.name} and l am {props.age} years old !</p>
         <p>{props.children}</p>  
+        <input type='text' onChange={props.changed} value = {props.name}/>
     </div>  
     )
 
